c5a3e19c3c263781d9084bf2a01941c67e0fd562
Palaklakhani04 <palaklakhani2021@gmail.com>
2025-07-30 12:26:00 +0530

    hello world

---
+++ express/app.js
@@ -0,0 +1,10 @@
+import express from "express";
+
+const app = express()
+
+app.get("/", (req, res) => res.send("<h1>Hello world!!</h1>"))
+
+const PORT = 3000;
+app.listen(PORT, () => {
+    console.log(`server is running at http://localhost:${PORT}/`)
+})
